ccbe0c268a511795bcdd1a764e52e3ce0033b386
Israel Ojeda <israel.ojeda@mybrexa.com>
2026-03-02 11:35:04 -0800

    Corrected OrdersController

---
+++ HelloWorldApiv2/Controllers/OrdersController.cs
@@ -0,0 +1,22 @@
+﻿using HelloWorldApiv2.BLL.Interfaces;
+using HelloWorldApiv2.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+
+namespace HelloWorldApiv2.Controllers
+{
+    [ApiController]
+    [Route("api/orders")]
+    public class OrdersController(IOrderService orderService) : ControllerBase
+    {
+        private readonly IOrderService OrderService = orderService;
+
+        [HttpGet]
+        [EnableQuery]
+        public ActionResult<IQueryable<OrderWithUserNameDto>> GetOrders()
+        {
+            return Ok(OrderService.GetOrdersWithUserName());
+        }
+
+    }
+}
